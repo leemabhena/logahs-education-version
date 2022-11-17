672e0381f5a362923564e7ab5edd28dd2ca4df6f
Lee Mabhena <lnm1@williams.edu>
2022-11-17 18:52:01 -0500

    Ready for upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,21 @@
 
-// navigation active toggler
+'use strict'
+
+// navigation
+const nav = document.querySelector('nav');
+
+nav.addEventListener('click', function(event){
+    console.log(event.target);
+})
+
+
+const removeActive = function(arr){
+    arr.forEach(element => {
+        element.classList.remove('active');
+    })
+}
+
+
+// footer date
+const dateEl = document.querySelector('.date');
+dateEl.textContent = new Date().getFullYear();
